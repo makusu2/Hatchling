5655515806d5ca39d30caf9878d93aeff9c96e9f
Maku <makusu2@gmail.com>
2018-04-24 14:23:19 -0400

    Improved container code

--- Assets/Scripts/ChestBehavior.cs
+++ Assets/Scripts/ChestBehavior.cs
@@ -7,133 +7,29 @@ using System.Linq;
 
 public class ChestBehavior : MonoBehaviour, ContainerInt {
 
-
-    
-    
-    
-    
     private GameObject player;
     private HUD hud;
-    private Inventory inv;
     
-    private Dictionary<string,int> itemCounts = new Dictionary<string,int>();
-    private Dictionary<string,GameObject> visibleBoxes = new Dictionary<string,GameObject>();
-    
-    
-    
-    
-    public int CountOf(string item) {
-        try {
-            return itemCounts[item];
-        }
-        catch(KeyNotFoundException){
-            return 0;
-        }
-    }
+    protected ContainerCls container;
+
+    public int CountOf(string item) { return container.CountOf(item);}
+    public void ItemMovedTo(string item) { container.ItemMovedTo(item);}
+    public void ItemTakenFrom(string item) { container.ItemTakenFrom(item);}
+    public void CreateIcons() { container.CreateIcons();}
+    public void DestroyIcons() { container.DestroyIcons();}
+    public void AddItem(string name) { container.AddItem(name);}
+    public void RemoveItem(string name) { container.RemoveItem(name);}
+    public void PrepareEntityInventory(string loot) { container.PrepareEntityInventory(loot);}
     
     
-    
-    void Awake() {
-		player = GameObject.FindWithTag("MainPlayer");
-    }
-    
-    
-	// Use this for initialization
-	void Start () {
-        hud = player.GetComponent<HUD>();
-        inv = hud.Inventory;
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    
-    
-    public void ItemMovedTo(string item) {
-        int numItemsToMove = inv.CountOf(item);//TODO ask user how many to move
-        for (int i=0;i<numItemsToMove;i++) {
-            inv.RemoveItem(item);
-            this.AddItem(item);
-        }
-    }
-    
-    public void ItemTakenFrom(string item) {
-        int numItemsToMove = CountOf(item);
-        for (int i=0;i<numItemsToMove;i++) {
-            inv.AddItem(item);
-            this.RemoveItem(item);
-        }
-    }
-    
-    
-    public void CreateIcons() {
-        DestroyIcons();
-        foreach(string item in itemCounts.Keys) {
-            AddBox(item);
-            //itemCounts[item] = itemCounts[item];
-            visibleBoxes[item].transform.Find("Text").GetComponent<Text>().text = ""+itemCounts[item];
-        }
-    }
-    public void DestroyIcons() {
-        string[] items = visibleBoxes.Keys.ToArray();
-        foreach(string item in items) {
-            GameObject icon = visibleBoxes[item];
-            Destroy(icon);
-            visibleBoxes.Remove(item);
-        }
-        //visibleBoxes = new Dictionary<string,GameObject>();
-    }
-    
-    
-    
-    public void AddItem(string name) {
-        if (!itemCounts.ContainsKey(name)) {
-            itemCounts[name] = 0;
-        }
-        itemCounts[name] += 1;
-        if(!visibleBoxes.ContainsKey(name)) {
-            AddBox(name);
-        }
-        visibleBoxes[name].transform.Find("Text").GetComponent<Text>().text = ""+itemCounts[name];
-    }
-    
-    public void RemoveItem(string name) {
-        try {
-            itemCounts[name] -= 1;
-            visibleBoxes[name].transform.Find("Text").GetComponent<Text>().text = ""+itemCounts[name];
-            if(itemCounts[name] == 0) {
-                itemCounts.Remove(name);
-                Destroy(visibleBoxes[name]);
-                visibleBoxes.Remove(name);
-            }
-        }
-        catch (KeyNotFoundException) {
-            Debug.Log("Attempted to remove item "+name+", but did not have any");
-        }
+    void Start() {
+        container = gameObject.AddComponent<ContainerCls>();
+        container.Prepare();
+        player = player??GameObject.FindWithTag("MainPlayer");
+        hud = hud??player.GetComponent<HUD>();
         
     }
     
-    Sprite GetSprite(string name) {
-        Sprite t = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Resources/InventoryIcons/"+name+".png", typeof(Sprite));
-        if (t == null) {
-            Debug.LogError("Could not find sprite for "+name);
-            t = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Resources/InventoryIcons/unknown.png", typeof(Sprite)); //Draw an unknown thing
-        }
-        return t;
-    }
-    
-    void AddBox(string name){
-        GameObject box = Instantiate(Resources.Load("InventoryIcons/BoxFab") as GameObject);
-        Sprite spr = GetSprite(name);
-        box.GetComponent<Image>().sprite = spr;
-        box.name = name+"Box";
-        box.transform.GetComponent<InfoHover>().infoStr = name;
-        box.transform.SetParent(hud.ChestPanel.transform,false);
-        box.transform.GetComponent<InventoryIconBehavior>().Item = name;
-        visibleBoxes[name] = box;
-    }
     
     
     
--- Assets/Scripts/LivingEntity.cs
+++ Assets/Scripts/LivingEntity.cs
@@ -22,6 +22,8 @@ public class LivingEntity : MonoBehaviour, ContainerInt{
     
     protected UnityEngine.AI.NavMeshAgent nav;
     
+    protected ContainerCls container;
+    
     protected Animator ani;
     
     protected GameObject mouthGO;
@@ -132,6 +134,8 @@ public class LivingEntity : MonoBehaviour, ContainerInt{
         player = player??GameObject.FindWithTag("MainPlayer");
         hud = hud??player.GetComponent<HUD>();
         TestForNearbyTargets();
+        container = gameObject.AddComponent<ContainerCls>();
+        container.Prepare();
         PrepareEntityInventory(drop);
     }
     
@@ -326,125 +330,18 @@ public class LivingEntity : MonoBehaviour, ContainerInt{
         fireball.GetComponent<FireballBehavior>().GetShot(transform.forward);
     }
 
-
+    public int CountOf(string item) { return container.CountOf(item);}
+    public void ItemMovedTo(string item) { container.ItemMovedTo(item);}
+    public void ItemTakenFrom(string item) { container.ItemTakenFrom(item);}
+    public void CreateIcons() { container.CreateIcons();}
+    public void DestroyIcons() { container.DestroyIcons();}
+    public void AddItem(string name) { container.AddItem(name);}
+    public void RemoveItem(string name) { container.RemoveItem(name);}
+    public void PrepareEntityInventory(string loot) { container.PrepareEntityInventory(loot);}
     
     
     
     
     
-    private Dictionary<string,int> itemCounts = new Dictionary<string,int>();
-    private Dictionary<string,GameObject> visibleBoxes = new Dictionary<string,GameObject>();
     
-    
-    
-    
-    public int CountOf(string item) {
-        try {
-            return itemCounts[item];
-        }
-        catch(KeyNotFoundException){
-            return 0;
-        }
-    }
-    
-    
-    public void ItemMovedTo(string item) {
-        int numItemsToMove = hud.Inventory.CountOf(item);//TODO ask user how many to move
-        for (int i=0;i<numItemsToMove;i++) {
-            hud.Inventory.RemoveItem(item);
-            this.AddItem(item);
-        }
-    }
-    
-    public void ItemTakenFrom(string item) {
-        int numItemsToMove = CountOf(item);
-        for (int i=0;i<numItemsToMove;i++) {
-            hud.Inventory.AddItem(item);
-            this.RemoveItem(item);
-        }
-    }
-    
-    
-    public void CreateIcons() {
-        DestroyIcons();
-        foreach(string item in itemCounts.Keys) {
-            AddBox(item);
-            //itemCounts[item] = itemCounts[item];
-            visibleBoxes[item].transform.Find("Text").GetComponent<Text>().text = ""+itemCounts[item];
-        }
-    }
-    public void DestroyIcons() {
-        string[] items = visibleBoxes.Keys.ToArray();
-        foreach(string item in items) {
-            GameObject icon = visibleBoxes[item];
-            Destroy(icon);
-            visibleBoxes.Remove(item);
-        }
-        //visibleBoxes = new Dictionary<string,GameObject>();
-    }
-    
-    
-    
-    public void AddItem(string name) {
-        print("Adding "+name);
-        if (!itemCounts.ContainsKey(name)) {
-            print(name+" wasn't in itemCounts, adding...");
-            itemCounts[name] = 0;
-        }
-        itemCounts[name] += 1;
-        if(!visibleBoxes.ContainsKey(name)) {
-            AddBox(name);
-        }
-        visibleBoxes[name].transform.Find("Text").GetComponent<Text>().text = ""+itemCounts[name];
-    }
-    
-    public void RemoveItem(string name) {
-        try {
-            itemCounts[name] -= 1;
-            visibleBoxes[name].transform.Find("Text").GetComponent<Text>().text = ""+itemCounts[name];
-            if(itemCounts[name] == 0) {
-                itemCounts.Remove(name);
-                Destroy(visibleBoxes[name]);
-                visibleBoxes.Remove(name);
-            }
-        }
-        catch (KeyNotFoundException) {
-            Debug.Log("Attempted to remove item "+name+", but did not have any");
-        }
-        
-    }
-    
-    Sprite GetSprite(string name) {
-        Sprite t = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Resources/InventoryIcons/"+name+".png", typeof(Sprite));
-        if (t == null) {
-            Debug.LogError("Could not find sprite for "+name);
-            t = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Resources/InventoryIcons/unknown.png", typeof(Sprite)); //Draw an unknown thing
-        }
-        return t;
-    }
-    
-    void AddBox(string name){
-        GameObject box = Instantiate(Resources.Load("InventoryIcons/BoxFab") as GameObject);
-        Sprite spr = GetSprite(name);
-        box.GetComponent<Image>().sprite = spr;
-        box.name = name+"Box";
-        box.transform.GetComponent<InfoHover>().infoStr = name;
-        box.transform.SetParent(hud.ChestPanel.transform,false);
-        box.transform.GetComponent<InventoryIconBehavior>().Item = name;
-        visibleBoxes[name] = box;
-    }
-    
-    
-    
-    void PrepareEntityInventory(string loot) {
-        string[] components = loot.Split(',');
-        foreach(string comp in components) {
-            int count = int.Parse(comp.Split('*')[0]);
-            string compMat = comp.Split('*')[1];
-            for (int i=0;i<count;i++) {
-                AddItem(compMat);
-            }
-        }
-        DestroyIcons();
-    }
 }
