2e7578f40d88010125f4b357d3a7001bc4c6473f
Maku <makusu2@gmail.com>
2018-06-05 06:45:30 -0400

    Ugh git

--- Assets/Scripts/Heated.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Heated : MonoBehaviour {
-
-    public int HeatLevel = 0;
-	// Use this for initialization
-}
